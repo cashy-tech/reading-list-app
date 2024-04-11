ad44210a5903e907eafed4b5febb2b2cdebe3dc8
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-11 15:32:50 +0300

    Adding a book

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import BookCreate from "./components/book-create";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const handleCreate = (title) => {};
+  const handleCreate = (newtitle) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 999), title: newtitle },
+    ];
+    setBooks(updatedBooks);
+  };
   return (
     <div>
       <BookCreate onCreate={handleCreate} />
